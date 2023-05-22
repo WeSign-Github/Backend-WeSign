bb51f9271526d541f9c79c19a7e4d00be50d12b2
zakihaha <muhzakich@students.amikom.ac.id>
2023-05-22 19:38:36 +0700

    validator register

---
+++ app/api/auth/validator.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+const validateRequest = require("../../utils/validateRequest");
+
+module.exports = async (req, res, next) => {
+    const schema = Joi.object({
+        provider_id: Joi.string()
+            .required(),
+
+        provider_name: Joi.string()
+            .required(),
+
+        first_name: Joi.string()
+            .min(3)
+            .max(30)
+            .required(),
+
+        last_name: Joi.string()
+            .min(2)
+            .max(30)
+            .required(),
+
+        email: Joi.string()
+            .email({ tlds: { allow: true } })
+            .required(),
+    });
+
+    validateRequest(req, next, schema);
+}
