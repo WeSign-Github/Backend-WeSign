cb8ce139f70d47d438a7e1c1fa96d5b1ff677699
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2023-05-22 22:19:06 +0700

    refactor

--- app/api/lessons/controller.js
+++ app/api/lessons/controller.js
@@ -1,69 +1,29 @@
-const { Lesson, Progress_User, Lesson_User } = require('../../db/models')
+const { StatusCodes } = require('http-status-codes');
+const getOneLesson = require('./handler/getOneLesson');
+const completeTheLesson = require('./handler/completeTheLesson');
 
-const show = async (req, res) => {
-    const { id } = req.params
+const show = async (req, res, next) => {
+    try {
+        let result = await getOneLesson(req)
 
-    const lesson = await Lesson.findByPk(id);
-
-    res.status(200).json({
-        data: lesson
-    });
+        res.status(StatusCodes.OK).json({
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
-const completeLesson = async (req, res) => {
-    const { id } = req.params
+const completeLesson = async (req, res, next) => {
+    try {
+        let result = await completeTheLesson(req)
 
-    const lesson = await Lesson.findByPk(id);
-    const user_already_completed = await lesson.hasUser(1)
-
-    if (!user_already_completed) {
-        await Lesson_User.create({
-            user_id: 1,
-            course_id: lesson.course_id,
-            lesson_id: lesson.id
+        res.status(StatusCodes.OK).json({
+            data: result
         })
-
-        const lessons = await Lesson.findAll({
-            where: {
-                course_id: lesson.course_id
-            }
-        })
-
-        const total_lessons_completed = await Lesson_User.count({
-            where: {
-                course_id: lesson.course_id,
-                user_id: 1
-            }
-        })
-
-        const total_lesson = lessons.length
-        const learning_percentage = total_lessons_completed / total_lesson * 100
-
-        const is_user_progress_exist = await Progress_User.findOne({
-            where: {
-                user_id: 1,
-                course_id: lesson.course_id
-            }
-        })
-
-        if (is_user_progress_exist) {
-            await is_user_progress_exist.update({
-                lesson_id: lesson.id,
-                learning_percentage
-            })
-        } else {
-            await Progress_User.create({
-                user_id: 1,
-                course_id: lesson.course_id,
-                lesson_id: lesson.id,
-                learning_percentage
-            })
-        }
+    } catch (error) {
+        next(error)
     }
-
-    res.status(200).json({
-        message: 'Lesson completed'
-    });
 }
 
 module.exports = { show, completeLesson }
