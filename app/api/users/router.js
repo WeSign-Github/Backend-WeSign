0b2b04bc0fbd4f23c0d10213b67af053bfd9a725
zakihaha <muhzakich@students.amikom.ac.id>
2023-05-22 22:45:58 +0700

    refactor

--- app/api/users/router.js
+++ app/api/users/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../../middleware/auth');
+
 const { me, learningProgress } = require('./controller')
 
-router.get('/me', me);
-router.get('/learning-progress', learningProgress);
+router.get('/me', auth, me);
+router.get('/learning-progress', auth, learningProgress);
 
 module.exports = router;
